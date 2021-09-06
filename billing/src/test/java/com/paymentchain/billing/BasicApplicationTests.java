66d1c07094dc3ca63ccd87538fe49d43029773ef
Gino Luciano Rojo <46763206+GinoLucianoRojo1993@users.noreply.github.com>
2021-09-06 11:40:49 -0300

    Modificacion pequeña

--- billing/src/test/java/com/paymentchain/billing/BasicApplicationTests.java
+++ billing/src/test/java/com/paymentchain/billing/BasicApplicationTests.java
@@ -1,5 +1,5 @@
 package com.paymentchain.billing;
-
+import org.unit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -11,6 +11,8 @@ public class BasicApplicationTests {
 
 	@Test
 	public void contextLoads() {
+	  String meesage = "Probando CI"
+	  Assert.assertNotNull(meesage);
 	}
 
 }
